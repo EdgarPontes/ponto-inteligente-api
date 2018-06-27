1902d12768a1fa3c762487fb550587699f75263f
Edgar Pontes <edgarponte@gmail.com>
2018-06-26 22:10:29 -0300

    correcao

--- src/main/java/br/com/makewaybrazil/pontointeligente/PontoInteligenteApplication.java
+++ src/main/java/br/com/makewaybrazil/pontointeligente/PontoInteligenteApplication.java
@@ -1,12 +1,12 @@
-package br.com.makewaybrazil.pontointeligente;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class PontoInteligenteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PontoInteligenteApplication.class, args);
-	}
-}
+//package br.com.makewaybrazil.pontointeligente;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class PontoInteligenteApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(PontoInteligenteApplication.class, args);
+//	}
+//}
